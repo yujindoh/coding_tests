3d667cec6e8cf989280d6837e35f480ea3c174bf
yujindoh <43058331+yujindoh@users.noreply.github.com>
2020-05-13 12:59:27 +0900

    Create 짝수와 홀수.py

---
+++ Level 1/짝수와 홀수.py
@@ -0,0 +1,7 @@
+# https://programmers.co.kr/learn/courses/30/lessons/12937
+# 프로그래머스는 내가 코드 짜는거보다 다른 사람들의 간결한 코드 보는게 더 신기한 듯... 볼때마다 더 간단하게 코드 짜는 법을 공부해야겠다고 생각한다.
+def solution(num):
+    if num%2==1:
+        return 'Odd'
+    else:
+        return 'Even'

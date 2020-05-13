5a6ff3b7eef0192fd0ab6a4b545576b6e5ab92e2
yujindoh <43058331+yujindoh@users.noreply.github.com>
2020-05-13 17:40:47 +0900

    Create 평균구하기.py

---
+++ Level 1/평균구하기.py
@@ -0,0 +1,6 @@
+#https://programmers.co.kr/learn/courses/30/lessons/12944
+#sum 함수를 쓰면 더 간단하게 할 수 있다더라...하하...
+for j in range(len(i)):
+    empty = []
+    empty = empty.append(str(i[j]))
+    print(empty)
